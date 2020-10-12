af548cdf2c3b43fac1b4465fba4c384260e81d53
Matheus Richard Chignolli <matheuschignolli@gmail.com>
2020-10-12 10:20:07 -0300

    migration

--- src/database/migrations/1598566797643-CreateAppointments.ts
+++ src/database/migrations/1598566797643-CreateAppointments.ts
@@ -1,9 +1,4 @@
-import {
-  MigrationInterface,
-  QueryRunner,
-  Table,
-  TableForeignKey,
-} from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateAppointments1598566797643
   implements MigrationInterface {
