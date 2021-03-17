d9583daeaa5ca86df86ba5c240b9d652f903e891
Matheus Richard Chignolli <matheuschignolli@gmail.com>
2021-03-16 22:03:50 -0300

    fix: variable name

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-import AuthneticateUserService from '@modules/users/services/AuthenticateUserService';
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 export default class SessionsController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
 
-    const authenticateUser = container.resolve(AuthneticateUserService);
+    const authenticateUser = container.resolve(AuthenticateUserService);
 
     const { user, token } = await authenticateUser.execute({
       email,
